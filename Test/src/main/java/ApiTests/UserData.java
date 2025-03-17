9b14619af74a9a841109fbb5c4c176c0b792a7ca
freezedWaR3D <113523459+freezedWaR3D@users.noreply.github.com>
2025-03-17 14:22:49 +0300

    1

---
+++ Test/src/main/java/ApiTests/UserData.java
@@ -0,0 +1,49 @@
+package ApiTests;
+
+public class UserData {
+    public Integer userId;
+    public Integer id;
+    public String title;
+    public String body;
+
+    public UserData() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
+
+
+
+
+
+
